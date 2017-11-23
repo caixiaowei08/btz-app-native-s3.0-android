2a08e465a6e3bc09083b457ced52ef5f225f389b
caixiaowei <caixiaowei@caixiaoweideMacBook-Pro.local>
2017-11-23 22:14:42 +0800

    20171123

---
+++ app/src/main/java/com/btz/activity/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.btz.activity.splash;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.btz.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/com/btz/activity/user/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.btz.activity.user;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.btz.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
